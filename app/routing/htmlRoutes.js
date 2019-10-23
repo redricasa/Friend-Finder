43760f6f0db58b17b997d9be89e6568a49394d7f
redricasa <fredeb26@gmail.com>
2019-10-22 19:02:49 -0700

    set up server.js with body-parser, express. npm installed body-perser

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,10 @@
-// ur htmlRoutes.js file should include two routes:
-
+var path = require('path');
 // A GET Route to /survey which should display the survey page.
-//TO DO
+module.exports = function (app){
+    app.get("/survey", function(request, response){
+        response.sendFile(path.join(__dirname + "/../public/survey.html"));
+    });
+
+};
 // A default, catch-all route that leads to home.html which displays the home page.
 //TO DO
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended:false});
+// parse different custom JSON types as JSON
+app.use(bodyParser.json({type: 'application/*+json'}))
+
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}));
+
+app.use(bodyParser.text({ type: 'text/html' }))
+
+app.listen(PORT, function(){
+    console.log("app listening on port: " + PORT)
+});
