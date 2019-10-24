a710d19ce426546f9d37b86311bd20be867832bd
redricasa <fredeb26@gmail.com>
2019-10-23 17:50:45 -0700

    added a jquery DOM for the submit button

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
-
-var jsonParser = bodyParser.json();
-var urlencodedParser = bodyParser.urlencoded({ extended:false});
+app.use(bodyParser.urlencoded({ extended: true}));
 // parse different custom JSON types as JSON
 app.use(bodyParser.json({type: 'application/*+json'}))
 
