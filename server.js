d3b72a01ad2a523a82d8598c9398d1976b8f926f
redricasa <fredeb26@gmail.com>
2019-10-23 23:15:06 -0700

    when inspecting the broken photo in dev tools, it was discovered that the location of the photo is /photo/.png. we needed to serve up static assets

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var path = require('path')
 var app = express();
 var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -12,6 +12,9 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}));
 app.use(bodyParser.text({ type: 'text/html' }))
 require("./app/routing/apiRoutes.js")(app)
 require("./app/routing/htmlRoutes.js")(app)
+//creates a route that serves the photoes
+app.use('/photoes', express.static(path.join(__dirname, 'app/photoes')))
+
 app.listen(PORT, function(){
     console.log("app listening on port: " + PORT)
 });
